ba03efdf3c6e4ba2d26473fad62ae403dcd6a910
jamie-cloud99 <j1999j88@gmail.com>
2023-09-04 21:33:28 +0800

    feat: 新增卡片延伸連結

--- src/components/NewsComponent.vue
+++ src/components/NewsComponent.vue
@@ -7,7 +7,7 @@
   <hr class="border-white" />
   <ul class="mb-[56px] grid-cols-2 font-bold lg:mb-[92px] lg:grid">
     <li class="col-span-2">
-      <div class="border-b border-white px-4 py-8 lg:py-10">
+      <div class="relative border-b border-white px-4 py-8 lg:py-10">
         <p class="tracking-1 mb-2">
           <span class="material-symbols-outlined align-bottom">
             emergency
@@ -23,19 +23,15 @@
             </p>
           </div>
           <div class="col-span-3 justify-self-end">
-            <a href="#">
-              <img
-                src="/images/line_end_arrow_notch2.png"
-                alt="arrow"
-                class=""
-              />
+            <a href="#" class="stretched-link hover:opacity-50">
+              <img src="/images/line_end_arrow_notch2.png" alt="arrow" />
             </a>
           </div>
         </div>
       </div>
     </li>
     <li class="col-span-1">
-      <div class="border-b border-white px-4 py-8 lg:py-10">
+      <div class="relative border-b border-white px-4 py-8 lg:py-10">
         <p class="tracking-1 mb-2">
           <span class="material-symbols-outlined align-bottom">
             emergency
@@ -47,13 +43,15 @@
           為答謝廣大用戶的支持，本月將舉辦一場限時優惠活動。訂閱 ALPHABOX+
           的顧客，皆可享受九折優惠。機會難得，請勿錯過此良機！
         </p>
-        <a href="#">
-          <img src="/images/line_end_arrow_notch2.png" alt="arrow" class="" />
+        <a href="#" class="stretched-link hover:opacity-50">
+          <img src="/images/line_end_arrow_notch2.png" alt="arrow" />
         </a>
       </div>
     </li>
     <li class="col-span-1">
-      <div class="border-b border-white px-4 py-8 lg:border-l lg:py-10">
+      <div
+        class="relative border-b border-white px-4 py-8 lg:border-l lg:py-10"
+      >
         <p class="tracking-1 mb-2">
           <span class="material-symbols-outlined align-bottom">
             emergency
@@ -65,8 +63,8 @@
           為使您的 ALPHABOX+
           更趨完善，我們不懈地研發創新功能。近期，我們新推出智慧家居控制功能，使您的機器人能更便利地協助您操作家中各類智能設備。
         </p>
-        <a href="#">
-          <img src="/images/line_end_arrow_notch2.png" alt="arrow" class="" />
+        <a href="#" class="stretched-link hover:opacity-50">
+          <img src="/images/line_end_arrow_notch2.png" alt="arrow" />
         </a>
       </div>
     </li>
